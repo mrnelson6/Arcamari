46633b9fb06eff12776ae5028bd44374c3e44790
Matthew Nelson <matt9678@esri.com>
2019-01-11 17:31:59 -0800

    Collision detection

--- src/display-device-location/src/main/java/com/esri/arcgisruntime/sample/displaydevicelocation/GameRunner.java
+++ src/display-device-location/src/main/java/com/esri/arcgisruntime/sample/displaydevicelocation/GameRunner.java
@@ -1,6 +1,7 @@
 package com.esri.arcgisruntime.sample.displaydevicelocation;
 
 import java.util.List;
+import java.lang.Math;
 import java.util.ArrayList;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
 import com.esri.arcgisruntime.mapping.view.MapView;
@@ -9,6 +10,9 @@ import com.esri.arcgisruntime.data.QueryParameters;
 import com.esri.arcgisruntime.data.FeatureQueryResult;
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
 import com.esri.arcgisruntime.data.Feature;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.location.LocationDataSource.Location;
+
 
 
 public class GameRunner {
@@ -19,6 +23,9 @@ public class GameRunner {
 
     public void mainLoop() {
         ArcGISMap map = mMapView.getMap();
+
+        List<Item> items = new ArrayList();
+
         List<FeatureTable> ft = map.getTables();
         //iterate over features and check for collisions
         List<Feature> features = new ArrayList();
@@ -33,6 +40,26 @@ public class GameRunner {
                 }
             } catch(Exception e) {
                 System.out.println("what happened!");
+            }
+        }
+
+        Location playerLoc = mMapView.getLocationDisplay().getLocation();
+        Point playerPt = playerLoc.getPosition();
+        double arbDiam = 10;
+        Player player = new Player(playerPt.getX(), playerPt.getY(), arbDiam);
+        for(Item currItem : items) {
+            //check collision
+            if(Math.sqrt(Math.pow((player.getLat() - currItem.getLatitude()),2) +
+                    Math.pow((player.getLon() - currItem.getLongitude()),2)) <
+                    (player.getDiameter() + currItem.getDiameter())) {
+                ListenableFuture<Void> future = currItem.getFeature().getFeatureTable().deleteFeatureAsync(currItem.getFeature());
+                try {
+                    future.get();
+                    items.remove(currItem);
+                } catch(Exception e) {
+                    System.out.println("I cannot begin to fathom how we got here!");
+                    System.out.println(e.getMessage());
+                }
             }
         }
         //wait one second possibly
